ae1114edb30b304b0c8a0c3f838961249e96c474
jaime <jaime.carrasc01995@gmail.com>
2025-03-04 10:54:51 +0100

    mas info en el detailActivity

--- app/src/main/java/com/example/horoscope/detailActivity.kt
+++ app/src/main/java/com/example/horoscope/detailActivity.kt
@@ -1,12 +1,24 @@
 package com.example.horoscope
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class detailActivity : AppCompatActivity() {
+
+    companion object{
+        const val HOROSCOPE_ID = "HOROSCOPE_ID"
+    }
+    lateinit var nameTextView: TextView
+    lateinit var dateTextView: TextView
+    lateinit var iconImageView: ImageView
+    lateinit var horoscope: Horoscope
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,7 +28,21 @@ class detailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val id =intent.getStringExtra("HOROSCOPE_ID")
 
+        val id =intent.getStringExtra(HOROSCOPE_ID)
+        val horoscope = Horoscope.findByID(id!!)
+        iniciarComponentes()
+        loadData()
+
+    }
+    private fun loadData(){
+        nameTextView.setText(horoscope.name)
+        dateTextView.setText(horoscope.date)
+        iconImageView.setImageResource(horoscope.icon)
+    }
+    fun iniciarComponentes(){
+        nameTextView = findViewById(R.id.nameTextView)
+        dateTextView = findViewById(R.id.dateTextView)
+        iconImageView = findViewById(R.id.iconImageView)
     }
 }
